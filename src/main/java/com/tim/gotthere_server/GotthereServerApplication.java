dd82f209fea83ac74183cf8b1ec502744c3e49d0
Tim <tjcichra@yahoo.com>
2020-09-27 11:13:23 -0400

    Added base files from Spring Initializr (including maven wrapper)

---
+++ src/main/java/com/tim/gotthere_server/GotthereServerApplication.java
@@ -0,0 +1,13 @@
+package com.tim.gotthere_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotthereServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotthereServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim/gotthere_server/GotthereServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.gotthere_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotthereServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

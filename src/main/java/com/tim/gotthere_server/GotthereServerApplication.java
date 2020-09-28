4bd862b3c794f9510ef04b152c96e1c72491c5e8
Tim <tjcichra@yahoo.com>
2020-09-27 20:47:23 -0400

    Added Thymeleaf and basic web page

--- src/main/java/com/tim/gotthere_server/GotthereServerApplication.java
+++ src/main/java/com/tim/gotthere_server/GotthereServerApplication.java
@@ -2,8 +2,9 @@ package com.tim.gotthere_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GotthereServerApplication {
 
 	public static void main(String[] args) {

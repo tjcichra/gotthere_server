58ae1ea1b5b1a975c56f0dcfd4106801be9bccf0
Tim <tjcichra@yahoo.com>
2020-10-25 14:06:57 -0400

    The connection now handles latitude and longitude with 6 decimal places. The SQL file has been updated to reflect this.

--- src/main/java/com/tim/gotthere_server/GotthereDatabase.java
+++ src/main/java/com/tim/gotthere_server/GotthereDatabase.java
@@ -42,7 +42,7 @@ public class GotthereDatabase implements CommandLineRunner {
 				InputStream input = socket.getInputStream()
 			) {
 				while(true) {
-					byte buffer[] = new byte[10];
+					byte buffer[] = new byte[15];
 					try {
 						if(input.read(buffer) != -1) {
 							double bearing = 0;
@@ -53,11 +53,10 @@ public class GotthereDatabase implements CommandLineRunner {
 							}
 							bearing = bearing + buffer[1];
 							bearing = bearing + (buffer[2] / 100d);
-							
 
-							double latitude = buffer[3] + (buffer[4] / 100d);
-							double longitude = buffer[5] + buffer[6] + (buffer[7] / 100d);
-							double speed = buffer[8] + (buffer[9] / 100d);
+							double latitude = buffer[3] + buffer[4] + (buffer[5] / 100d) + (buffer[6] / 10000d) + (buffer[7] / 1000000d);
+							double longitude = buffer[8] + buffer[9] + (buffer[10] / 100d) + (buffer[11] / 10000d) + (buffer[12] / 1000000d);
+							double speed = buffer[13] + (buffer[14] / 100d);
 
 							System.out.println("Bearing: " + bearing + " Latitude: " + latitude + " Longitude: " + longitude + " Speed: " + speed);
 

2019aee2ab88176b640d29391668776e6161296c
Tim <tjcichra@yahoo.com>
2020-11-27 11:45:47 -0500

    Updated stompjs to 6.0.0 and fixed database issues.

--- src/main/resources/static/scripts/websocket.js
+++ src/main/resources/static/scripts/websocket.js
@@ -1,68 +1,54 @@
 var stompClient = null;
-var map = null;
-var markers = [];
-var lastMarker = null;
-
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
 
 function connectWebsocket() {
-    var socket = new SockJS('/gs-guide-websocket');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        //setConnected(true);
+    var port = location.port;
+    if(port) {
+        port = ":" + port;
+    }
+
+    stompClient = new StompJs.Client({
+        brokerURL: "ws://" + window.location.hostname + port + "/web-socket-stomp",
+        debug: function (str) {
+            console.log(str);
+        },
+        reconnectDelay: 5000,
+        heartbeatIncoming: 4000,
+        heartbeatOutgoing: 4000,
+    });
+
+    stompClient.onConnect = function(frame) {
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
+
+        stompClient.subscribe('/web-socket/location', function (greeting) {
             var startDateTimeL = document.getElementById("startdatetime").value;
             var endDateTimeL = document.getElementById("enddatetime").value;
 
             var location = JSON.parse(greeting.body);
 
-            if(location.realDateTime >= startDateTimeL && location.realDateTime <= endDateTimeL) {
+            if(location.dateTime >= startDateTimeL && location.dateTime <= endDateTimeL) {
                 markLocation(location);
 
                 if(document.getElementById("follow").checked) {
                     centerMap(location.latitude, location.longitude);
                 }
             }
-            console.log(greeting);
+            console.log(greeting.body);
         });
-    });
+    };
+
+    stompClient.onStompError = function(frame) {
+        console.log('Broker reported error: ' + frame.headers['message']);
+        console.log('Additional details: ' + frame.body);
+    }
+
+    stompClient.activate();
 }
 
 function disconnect() {
     if (stompClient !== null) {
         stompClient.disconnect();
     }
-    setConnected(false);
     console.log("Disconnected");
-}
-
-//Gets the start and end date-times and sends them out as a JSON message to the server.
-function requestLocations() {
-    var startDateTimeL = document.getElementById("startdatetime").value;
-    var endDateTimeL = document.getElementById("enddatetime").value;
-
-    //Only continue if start date-time is before the end date-time.
-    if(startDateTimeL < endDateTimeL) {
-        var requestObject = {
-            startDateTime: startDateTimeL,
-            endDateTime: endDateTimeL
-        };
-
-        stompClient.send("/app/hello", {}, JSON.stringify(requestObject));
-    } else {
-        $("#locations").append("Cannot have start time be after end time.<br>");
-    }
 }
 
 $(function () {

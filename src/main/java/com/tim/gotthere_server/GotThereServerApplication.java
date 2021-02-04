dc10a35e15adfc7fa1438a21b556d91acad40e08
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-02-04 18:32:04 -0500

    Allow deployment as war file.

--- src/main/java/com/tim/gotthere_server/GotThereServerApplication.java
+++ src/main/java/com/tim/gotthere_server/GotThereServerApplication.java
@@ -2,9 +2,16 @@ package com.tim.gotthere_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GotThereServerApplication {
+public class GotThereServerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GotThereServerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GotThereServerApplication.class, args);
